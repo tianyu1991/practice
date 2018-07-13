b314227339901b9b1759a49f0bbd29dcbad1957b
tianyu liu <hulan1991@gmail.com>
2018-07-13 17:02:16 -0400

    Update code.r

--- code.r
+++ code.r
@@ -1,2 +1,6 @@
 x<-factor(c("yes","yes","no"),levels=c("yes","no"))
 names(x)<-c("a1","a2")
+
+intial<-read.table("datatable.txt",nrows=100)
+classes<-sapply(initial,class)
+tabAll<-read.table("data.txt",colClasses=classes)
