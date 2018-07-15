8fa7e281d2da64829762c783f38add95f63ae943
tianyu liu <hulan1991@gmail.com>
2018-07-15 15:22:47 -0400

    Update code.r

--- code.r
+++ code.r
@@ -9,3 +9,9 @@ tabAll<-read.table("data.txt",colClasses=classes)
  gather(df, key, value, -owner) %>%
 +   group_by(owner, key, value) %>%
 + summarize(n=n())
+
+a<-group_by(data,data$AgeOld)%>%
++ summarize(n=n())
+a<-as.data.frame(a)
+a<-rbind(a,c(1,0))
+names(a)<-c("AgeOld","Count")
